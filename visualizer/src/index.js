354213ef7918053d9a38eabec31ecc66defb52d6
Will <will.fs.martian@gmail.com>
2022-09-10 18:28:17 -0400

    Initial Commit

---
+++ visualizer/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './orbitalConfigurations'
+import { createConfiguration, orbtialLetters, fillOrbitals } from './orbitalConfigurations';
+import { v4 as uuidv4 } from 'uuid';
+
+class Config extends React.Component {
+  render(configuration) {
+    this.props = configuration
+
+    let energyLevels = [];
+
+    let n = 1;
+    configuration.levels.forEach(level => {
+      let orbitals = [];
+      for (const [configNum, configs] of Object.entries(level)) {
+        let allConfigs = [];
+        const configID = `${orbtialLetters[configNum]}${n}`
+        const classes = `config ${configID}`;
+        for (let l = 0; l < configs.length; l++) {
+          allConfigs.push(<div key={uuidv4()} className={classes}></div>);
+        }
+        orbitals.push(<div key={uuidv4()} className="configurations-container"><p className="config-label">{n}{orbtialLetters[configNum]}</p>{allConfigs}</div> );
+      }
+
+      energyLevels.push(
+        <div key={uuidv4()} className="level">
+          <p key={uuidv4()} className="level-label">Level { n }</p>
+          { orbitals }
+        </div>
+      )
+      n++;
+    })
+    return (
+      <div className="TEST">
+       { energyLevels }
+      </div>
+    )
+  }
+}
+
+const configRoot = ReactDOM.createRoot(
+  document.querySelector(".content-container")
+);
+
+configRoot.render(new Config().render(createConfiguration(1)));
+
+const renderOrbitals = () => {
+  const energyLevel = Math.max(document.querySelector("#energy-level").value, 1);
+  const config = createConfiguration(energyLevel);
+  const div = new Config().render(config);
+  configRoot.render(div);
+}
+
+document.querySelector("#energy-level").onchange = () => {
+  renderOrbitals();
+}
+
+document.querySelector("#num-electrons").onchange = (event) => {
+  event.target.value = Math.max(event.target.value, 1);
+  renderOrbitals()
+  setTimeout(() => {
+    fillOrbitals(document.querySelector("#num-electrons").value);
+  }, 0);
+}
---
+++ visualizer/src/orbitalConfigurations.js
@@ -0,0 +1,71 @@
+
+export const orbtialLetters = {
+    0: "s",
+    1: "p",
+    2: "d",
+    3: "f"
+}
+
+const orbitalFillOrder = [
+    '1s', '2s', '2p', '3s', '3p', '4s', '3d', '4p', '5s', '4d', '5p', '6s', '4f', '5d', '6p', '7s', '5f', '6d', '7p', '8s'
+];
+
+const createEnergyLevel = (n) => {
+    const orbitals = {}
+    for (let l = 0; l < n; l++) {
+        let orientations = [];
+        for (let m = -l; m <= l; m++) { 
+            orientations.push(m === -0? 0 : m);
+        }
+        orbitals[l] = orientations;
+    }
+    return orbitals;
+}
+
+export const createConfiguration = (numLevels) => {
+    const levels = [];
+    for (let level = 1; level <= numLevels; level++) {
+        levels.push(createEnergyLevel(level));
+    }
+    return {
+        levels: levels,
+        numLevels: numLevels
+    };
+}
+
+
+
+const fillOrbital = (numElectrons, configurations) => {
+    const _fillOrbital = (numElectrons, symbol) => {
+        for (const configuration in configurations) {
+            configurations[configuration].innerHTML += symbol;
+            // console.log(configuration);
+            numElectrons--;
+            if (numElectrons === 0)
+                return 0;
+        }
+        return numElectrons;
+    }
+
+    numElectrons =  _fillOrbital(numElectrons, "↑");
+    if (numElectrons == 0)
+        return 0;
+    return _fillOrbital(numElectrons, "↓");
+}
+
+const clearConfigurations = (configurations) => {
+    configurations.forEach(configuration => {
+        configuration.innerHTML = "";
+    })
+}
+
+export const fillOrbitals = (numElectrons) => {
+    for (let orbital in orbitalFillOrder) {
+        orbital = orbitalFillOrder[orbital];
+        const configurations = Array.from(document.querySelectorAll(`.${orbital[1]}${orbital[0]}`));
+        clearConfigurations(configurations);
+        numElectrons = fillOrbital(numElectrons, configurations);
+        if (numElectrons == 0)
+            return;
+    }
+}
